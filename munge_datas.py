336519196cc9db212eb1547f4635d392c6829f39
Sam <splavigne@gmail.com>
2019-03-15 18:00:25 -0400

    more updates

--- munge_datas.py
+++ munge_datas.py
@@ -14,6 +14,43 @@
 import json
 import os
 
+bad_images = [
+    "images/522932432.jpg",
+    "images/192125023.jpg",
+    "images/459298663.jpg",
+    "images/459314113.jpg",
+    "images/522932432.jpg",
+    "images/509710836.jpg",
+    "images/509709582.jpg",
+    "images/459314961.jpg",
+    "images/459314113.jpg",
+    "images/459298663.jpg",
+    "images/458281465.jpg",
+    "images/340057260.jpg",
+    "images/25874831.jpg",
+    "images/192135974.jpg",
+    "images/192125094.jpg",
+    "images/186504358.jpg",
+    "images/126411626.jpg",
+    "images/126420972.jpg",
+    "images/269834098.jpg",
+    "images/509709586.jpg",
+    "images/454796559.jpg",
+    "images/460231783.jpg",
+    "images/450489590.jpg",
+    "images/318163765.jpg",
+    "images/192136036.jpg",
+    "images/186504064.jpg",
+    "images/522932954.jpg",
+    "images/81556401.jpg",
+    "images/427601367.jpg",
+    "images/522917413.jpg"
+]
+
+bad_image_ids = [int(i.replace('images/', '').replace('.jpg', '')) for i in bad_images]
+print(bad_image_ids)
+
+
 with open("prices.json", "r") as infile:
     prices = json.load(infile)
 
@@ -54,6 +91,8 @@ for listing in data:
         if not os.path.exists("images/{}.jpg".format(image["id"])):
             continue
         if image["id"] not in all_images:
+            continue
+        if image["id"] in bad_image_ids:
             continue
         images.append((image["id"], all_images[image["id"]]))
 
